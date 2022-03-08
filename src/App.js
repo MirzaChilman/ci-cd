5effbae80b02859a5d370559f77d973d750a2a36
Mirza Chilman <mirzachilman@gmail.com>
2022-03-08 17:38:19 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ARABIAN TAMPAN
+          edit dari github pages
         </p>
         <a
           className="App-link"
